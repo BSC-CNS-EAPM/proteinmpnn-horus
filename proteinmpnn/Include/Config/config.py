e770ecf361b6b7667a64e9bc6e65075c6805bd0b
Christian Domínguez <christian.99dominguez@gmail.com>
2025-03-13 17:56:17 +0100

    Updated config!

--- proteinmpnn/Include/Config/config.py
+++ proteinmpnn/Include/Config/config.py
@@ -3,7 +3,9 @@ from HorusAPI import PluginConfig, VariableTypes, PluginVariable
 conda_environment = PluginVariable(
     id="plugin_conda_env",
     name="Conda Environment",
-    description="The conda environment name to use for the plugin.",
+    description="The conda environment name to use for the plugin. "
+    "Use this command to install the correct dependencies in the environment: "
+    "conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch",
     type=VariableTypes.STRING,
     defaultValue="proteinmpnn",
 )
